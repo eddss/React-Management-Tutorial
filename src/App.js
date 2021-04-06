0af4c1b48069cc9735c396d7341e403f325c0e0c
eddss <jrhydra@gmail.com>
2021-04-06 15:13:58 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="log">
+
+      </img>
+      <h2>
+        Management System
+      </h2>
+    </div>
+  );
+}
+
+export default App;
